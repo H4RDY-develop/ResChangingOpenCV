ef95b3bd2a479eba3133515a2a4a7d2bb45c7ec7
H4RDY-develop <h4rdy@rocketship.com>
2021-07-10 01:11:59 +0400

    Added needed includes

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-// Adding libraries | May be extended
+#include <string>
+#include <dirent.h>
+#include <filesystem>
 
+// Adding libraries | May be extended
 using namespace std;
 using namespace cv;
+
+
+void Start()
+{
+	
+}
+
+
+
+int main()
+{
+
+}
