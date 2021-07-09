5fbfba3843de5b8052c2749689eeab4b14716d94
H4RDY-develop <h4rdy@rocketship.com>
2021-07-10 01:14:41 +0400

    Testing some sh1t

--- Main.cpp
+++ Main.cpp
@@ -3,15 +3,26 @@
 #include <string>
 #include <dirent.h>
 #include <filesystem>
+#include <sys/types.h>
 
 // Adding libraries | May be extended
 using namespace std;
 using namespace cv;
 
 
-void Start()
+void Start(const char *path)
 {
-	
+	struct dirent* entry;
+	DIR* dir = opendir(path);
+
+	if (dir == NULL) {
+		return;
+	}
+	while ((entry = readdir(dir)) != NULL)
+	{
+		cout << entry->d_name << endl;
+	}
+	closedir(dir);
 }
 
 
