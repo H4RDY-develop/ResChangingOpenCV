8659e62632e9d445412ac8e98d5458b760317fbd
H4RDY-develop <h4rdy@rocketship.com>
2021-07-10 02:33:10 +0400

    Некие изменения, имеются ошибки.

--- Main.cpp
+++ Main.cpp
@@ -3,43 +3,59 @@
 #include <string>
 #include <dirent.h>
 #include <sys/types.h>
+#include <vector>
 
 // Adding libraries | May be extended
 using namespace std;
 using namespace cv;
 
-
-void Direct(const char *path)
+class Resize
 {
-	struct dirent* entry;
-	DIR* dir = opendir(path);
-
-	if (dir == NULL) {
-		return;
-	}
-	while ((entry = readdir(dir)) != NULL)
+public:
+	int value;
+	string path;
+	
+	void Direct(const char* path)
 	{
-		cout << entry->d_name << endl;
+		struct dirent* entry;
+		DIR* dir = opendir(path);
+		vector<string> dirlist;
+
+		if (dir == NULL) {
+			return;
+		}
+		while ((entry = readdir(dir)) != NULL)
+		{
+			cout << entry->d_name << endl;
+			dirlist.push_back(entry->d_name);
+		}
+
+		string names = dirlist[0, value];
+		if (names.substr(names.find_last_of(".") + 1) == "txt")
+		{
+			cout << "Yes";
+		}
+		closedir(dir);
 	}
-	closedir(dir);
-}
+
+};
+
 
 void Check()
 {
-	string fn = "filename.conf";
-	if (fn.substr(fn.find_last_of(".") + 1) == "conf")
-	{
-		cout << "yes";
-	}
-	else {
-		cout << "no";
-	}
+	
 }
 
 
 
 int main()
 {
-	Direct("C:/Users/windo/Desktop/xd");
-	Check();
+	Resize Def;
+	cout << "Количество изображений: ";
+	cin >> Def.value;
+	cout << "Путь к папке с изображениями: ";
+	getline(cin, Def.path);
+	const char* path = Def.path.c_str();
+	Def.Direct(Def.path);
+	
 }
