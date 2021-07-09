5e9c57fb285627eecd05ef4bb0191ad83d73f0e5
H4RDY-develop <h4rdy@rocketship.com>
2021-07-10 01:35:43 +0400

    Checking of file extensions and making a list of files

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,6 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 #include <dirent.h>
-#include <filesystem>
 #include <sys/types.h>
 
 // Adding libraries | May be extended
@@ -10,7 +9,7 @@ using namespace std;
 using namespace cv;
 
 
-void Start(const char *path)
+void Direct(const char *path)
 {
 	struct dirent* entry;
 	DIR* dir = opendir(path);
@@ -25,9 +24,22 @@ void Start(const char *path)
 	closedir(dir);
 }
 
+void Check()
+{
+	string fn = "filename.conf";
+	if (fn.substr(fn.find_last_of(".") + 1) == "conf")
+	{
+		cout << "yes";
+	}
+	else {
+		cout << "no";
+	}
+}
+
 
 
 int main()
 {
-
+	Direct("C:/Users/windo/Desktop/xd");
+	Check();
 }
